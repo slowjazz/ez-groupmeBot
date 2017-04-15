fb4a4f54b0060a0361d70fcd05a5a005cbf064d5
kenny li <kenny li>
2017-04-14 23:15:58 -0500

    updated request function

--- app.js
+++ app.js
@@ -11,19 +11,29 @@ const BOT_ID = process.env.BOT_ID
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.sendFile('index.html');
+app.get('/', function(req, res) {
+    res.sendFile('index.html');
 });
 
-app.post('/post', (req, res)=>{
-	console.log(req.body);
-	request('https://api.groupme.com/v3/messages/'+req.body.group_id+'/'+ req.body.id
-		+'/like?token='+ACCESSTOKEN, (error, response, body)=>{
-			console.log("Liked message in group "+req.body.group_id)
-		});
+app.post('/post', (req, res) => {
+    console.log(req.body);
+    var options = {
+        url: 'https://api.groupme.com/v3/messages/' + req.body.group_id + '/' + req.body.id + '/like?token=' + ACCESSTOKEN,
+        method: 'POST'
+    }
+
+    request(options, (error, response, body) => {
+        console.log('sent to: https://api.groupme.com/v3/messages/' + req.body.group_id + '/' + req.body.id + '/like?token=' + ACCESSTOKEN);
+        //console.log("response code: "+response.body)
+        if (!error) {
+            console.log("Liked message in group " + req.body.group_id);
+        } else {
+            console.log("Error in operation");
+        }
+
+    });
 });
 
-app.listen(port, function () {
-  console.log('Example app listening on port' + port);
+app.listen(port, function() {
+    console.log('Example app listening on port' + port);
 });
-
