b2f00bb078855624fcf388ad5b664eeb4256f384
kenny li <kenny li>
2017-04-14 22:18:42 -0500

    catch bot callback and added like function to server

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const request = require('request');
 const port = process.env.PORT || 8000;
 
 const ACCESSTOKEN = process.env.ACCESSTOKEN;
 const BOT_ID = process.env.BOT_ID
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.sendFile('index.html');
+});
 
 app.post('/', (req, res)=>{
-	console.log(req.body);
-})
+	request('https://api.groupme.com/v3/messages/'+req.body.group_id+'/'+ req.body.id
+		+'/like?token='+ACCESSTOKEN, (error, response, body)=>{
+			console.log("Liked message in group "+req.body.group_id)
+		});
+});
 
 app.listen(port, function () {
   console.log('Example app listening on port' + port);
-})
+});
 
-
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
