a4e928e1a8953604bcb35bf5cb249d1536d081bd
kenny li <kenny li>
2017-04-14 21:34:25 -0500

    added server skeleton and environment vars

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+const ACCESSTOKEN = process.env.ACCESSTOKEN;
+const BOT_ID = process.env.BOT_ID
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res)=>{
+	console.log(req.body);
+})
+
+app.listen(port, function () {
+  console.log('Example app listening on port ${port}!')
+})
+
+
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
