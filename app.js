67f8a6a0f85e93d9fd9e7a8d4d5518cf9e425d10
kenny li <kenny li>
2017-04-14 22:51:32 -0500

    editted body parser

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const request = require('request');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 
 const ACCESSTOKEN = process.env.ACCESSTOKEN;
 const BOT_ID = process.env.BOT_ID
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.bodyParser());
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
 });
 
 app.post('/post', (req, res)=>{
-	console.log(req.jsonBody);
 	console.log(req.body);
 	request('https://api.groupme.com/v3/messages/'+req.body.group_id+'/'+ req.body.id
 		+'/like?token='+ACCESSTOKEN, (error, response, body)=>{
